d7072eb9140ebd19c5c490cc7b69579564c3bc63
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-10-22 21:39:38 +0100

    Logout button implemented.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -34,5 +35,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin()
                 .defaultSuccessUrl("/home", true);
+
+        // This logout implementation was copied from https://ducmanhphan.github.io/2019-02-22-Logout-in-Spring-Boot/
+        // author: Manh Phan - 22/02/2019
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)        // set invalidation state when logout
+                .deleteCookies("JSESSIONID")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/403");
     }
 }
